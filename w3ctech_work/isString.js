780b60df3baae7cd1a7f175eb0a852fee364af8a
Moriarty <837001764@qq.com>
2015-06-16 20:55:59 +0800

    Signed-off-by: Moriarty <837001764@qq.com>

---
+++ w3ctech_work/isString.js
@@ -0,0 +1,10 @@
+function isString(arg) {
+	 //return Object.prototype.toString.call(arg)==="[object String]";按到这样更好
+    return Object.prototype.toString.call(arg).indexOf("String") !== -1;
+    
+}
+// 附加的，之前的阿里面试题
+// 判断函数的参数的类型
+function typeofArg(arg){
+	return Object.prototype.toString.call(arg).slice(8,-1).toLowerCase();
+}
